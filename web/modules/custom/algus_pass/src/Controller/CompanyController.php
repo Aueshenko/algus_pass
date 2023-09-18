cbeaf24da3133d3e7c437e399450f7adf5b9d482
Aueshenko <mr.gelpo@gmail.com>
2023-09-18 09:36:52 +0500

    Контроллер страницы без разделения на функции

--- web/modules/custom/algus_pass/src/Controller/CompanyController.php
+++ web/modules/custom/algus_pass/src/Controller/CompanyController.php
@@ -6,15 +6,17 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\user\Entity\User;
 use Drupal\taxonomy\Entity\Term;
 
-class CompanyController extends ControllerBase {
-  public function taxonomy_folders() {
+class CompanyController extends ControllerBase
+{
+  public function taxonomy_folders()
+  {
     \Drupal::service('page_cache_kill_switch')->trigger();
 
     // Здесь указываем машинное имя таксономии, которую хотим отобразить.
     $taxonomy_name = 'taxonomy_folders';
     $curr_user = User::load(\Drupal::currentUser()->id());
     $curr_user_id = \Drupal::currentUser()->id();
-    if($curr_user){
+    if ($curr_user) {
       $company = $curr_user->get('field_company')->target_id;
     }
     // Загружаем все термины данной таксономии и преобразуем их в массивы.
@@ -23,7 +25,7 @@ class CompanyController extends ControllerBase {
     $user_terms = \Drupal::database()
       ->select('pass_access', 'p')
       ->fields('p', ['entity_id'])
-      ->condition('p.user_id',$curr_user_id)
+      ->condition('p.user_id', $curr_user_id)
       ->execute()->fetchAll();
     //Перезаписать в массив просто айдишники
     $user_terms_massive = [];
@@ -36,14 +38,13 @@ class CompanyController extends ControllerBase {
       // Загружаем термин с помощью его ID, чтобы получить доступ к полю 'field_company'.
       $term_entity = Term::load($term->tid);
       // Проверяем, равно ли поле 'field_company' заданной компании.
-      if($term_entity && in_array($term_entity->id(), $user_terms_massive)){
+      if ($term_entity && in_array($term_entity->id(), $user_terms_massive)) {
         if ($term_entity->get('field_company')->target_id == $company) {
           $filtered_terms[] = $term;
         }
       }
     }
 
-    //Поднимаемся вверх выводим всю структуру
     foreach ($filtered_terms as $filtered_term){
       while($filtered_term->parents[0] != 0){
         $parent_id = $filtered_term->parents[0];
@@ -55,6 +56,7 @@ class CompanyController extends ControllerBase {
         }
       }
     }
+
     // Create an index for easy lookups
     $index = [];
     foreach ($filtered_terms as $term) {
@@ -65,7 +67,7 @@ class CompanyController extends ControllerBase {
       if ($term->depth == 1) {
         $children = $this->buildStructure($term, $index);
         if (empty($children)) {
-          $result_massive[] = $term->name;
+          $result_massive[$term->name] = [];
         } else {
           $result_massive[$term->name] = $children;
         }
@@ -79,13 +81,15 @@ class CompanyController extends ControllerBase {
     ];
   }
 
-  function buildStructure($term, $index) {
+  //Рекурсивно строим массив перед твиг со вложенностью
+  function buildStructure($term, $index)
+  {
     $structure = [];
     foreach ($index as $subterm) {
       if ($subterm->parents[0] == $term->tid) {
         $children = $this->buildStructure($subterm, $index);
         if (empty($children)) {
-          $structure[] = $subterm->name;
+          $structure[$subterm->name] = [];
         } else {
           $structure[$subterm->name] = $children;
         }
